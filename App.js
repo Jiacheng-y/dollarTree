6943ad1ba710cf4f0eb1fa087cfcd535fb61d1bc
ChryslineLim <97398848+ChryslineLim@users.noreply.github.com>
2022-05-29 19:55:13 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AuthScreen } from './src/screens/AuthScreen';
+import { BudgetScreen } from './src/screens/BudgetScreen';
+import { HomeScreen } from './src/screens/HomeScreen';
+import { InOutFlowScreen } from './src/screens/InOutFlowScreen';
+import { authState } from './src/firebase';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView> 
-        <AuthScreen />
-      </SafeAreaView>
+        <ScreenManager isSignedIn={authState()} />
     </NavigationContainer>
   );
+}
+
+const ScreenManager = ({isSignedIn}) => {
+  const Tab = createBottomTabNavigator();
+  if (isSignedIn) {
+    return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Budget" component={BudgetScreen} />
+      <Tab.Screen name="InOutFlow" component={InOutFlowScreen} />
+    </Tab.Navigator>
+    );
+  } else {
+    return <AuthScreen />
+  }
 }
