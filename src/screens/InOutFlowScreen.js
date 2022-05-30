af8a00219b5352a69e8d0e6e30d1efc0223c5141
ChryslineLim <97398848+ChryslineLim@users.noreply.github.com>
2022-05-30 20:27:23 +0800

    Update InOutFlowScreen.js

--- src/screens/InOutFlowScreen.js
+++ src/screens/InOutFlowScreen.js
@@ -1,38 +1,66 @@
-import { SafeAreaView, Text, StyleSheet, TextInput, FlatList, View } from "react-native";
-import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, TextInput, FlatList, View, Pressable } from "react-native";
+import React, { useState, useEffect } from 'react';
 import { addItem } from "../firebase";
 import { db, auth } from "../firebase";
-import { query, collection, doc, onSnapshot } from "firebase/firestore";
+import { query, collection, doc, onSnapshot, deleteDoc } from "firebase/firestore";
+import { Item } from "../Components/Item";
 
 export const InOutFlowScreen = () => {
     const [list, setList] = useState([]);
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState(0);
 
-    const q = query(collection(db, "users", `${auth.currentUser.uid}`, "Expenses"));
-    onSnapshot(q, (snapshot) => {
-        let items = [];
-        snapshot.forEach((doc) => {
-            items.push(doc.data().expense); 
+    useEffect(() => {
+        const q = query(collection(db, "users", `${auth.currentUser.uid}`, "Expenses"));
+        const subscriber = onSnapshot(q, (snapshot) => {
+            const items = [];
+            snapshot.forEach((doc) => {
+                items.push({
+                    id: doc.id,
+                    ... doc.data()
+                }); 
+            })
+            setList(items);
         })
-        setList(items);
-    })
+        return subscriber;
+    }, []);
+
+    const deleteItem = async (id) => {
+        await deleteDoc(doc(db, "users", `${auth.currentUser.uid}`, "Expenses", `${id}`));
+    }
 
     return (
         <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
             <TextInput
                 style={styles.inputBox}
-                onSubmitEditing={ (text) => { 
-                    addItem(text.nativeEvent.text, "Expenses");
-                } }
-                placeholder='Add expense'>
+                onChangeText={setDescription}
+                value={description}
+                placeholder='Description'>
             </TextInput>
+            <TextInput
+                style={styles.inputBox}
+                placeholder='Amount'
+                onChangeText={setAmount}
+                value={amount}>
+            </TextInput>
+            <Pressable
+                style={styles.button}
+                onPress={() => { 
+                    addItem({category: description, amount: amount}, "Expenses");
+                    setDescription('');
+                    setAmount('');
+                }}>
+                <Text style={{fontSize: 20}}>Add</Text>
+            </Pressable>
             <FlatList
                 data={list}
-                renderItem={({item}) => (
-                    <View style = {styles.entries}> 
-                        <Text style={{fontSize: 20}}>{item}</Text>
-                    </View>
-                    
-            )} 
+                renderItem={({item, index}) => (
+                    <Item
+                        data={item}
+                        key={index}
+                        onDelete={deleteItem}>
+                    </Item>
+                )} 
             />
         </SafeAreaView>
     )
@@ -47,7 +75,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     inputBox: {
-        marginTop: 20,
+        marginTop: 10,
         backgroundColor: '#eef5ff',
         height: 55,
         width: 350,
@@ -56,5 +84,15 @@ const styles = StyleSheet.create({
         fontSize: 20,
         padding: 15, 
         borderColor: 'black'
+    },
+    button: {
+        height: 55,
+        width: 150,
+        backgroundColor: '#eef5ff',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 33,
+        marginVertical: 10
     }
 })
