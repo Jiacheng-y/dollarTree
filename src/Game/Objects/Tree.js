b7236b434fea68bc76d3d961e6e33dd8db3fde3d
Jiacheng <e0773814@u.nus.edu>
2022-07-26 14:00:02 +0800

    used animation as a temporary fix for coin falling from trees

--- src/Game/Objects/Tree.js
+++ src/Game/Objects/Tree.js
@@ -1,20 +1,74 @@
-import React from "react";
-import { Dimensions, Image } from 'react-native';
 import { TouchableOpacity } from "react-native";
 import { gardenImages } from "../../Images/Garden/gardenImages";
+import React, { useState, useEffect, useRef } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Pressable,
+    Dimensions,
+    ImageBackground,
+    Image,
+    StatusBar,
+    Platform,
+    FlatList,
+    Alert,
+    Animated
+} from 'react-native';
 
 export default Tree = (props) => {
 
     const tree = gardenImages[props.tree];
 
-    // return the Tree component as it should look
+
+    // For coin animation
+    const [show, setShow] = useState(false);
+    const coinImage = require("../../../assets/rptrz-ps-transparent-800x400.gif");
+    const befX = Dimensions.get('window').width;
+    const moveAnim = useRef(new Animated.ValueXY({x: befX / 2, y: 0})).current; 
+    const X = befX/ 2;
+    const Y = 500; 
+
+    const move = (value) => {
+        Animated.timing(moveAnim, {
+            toValue: {x: X, y: Y},
+            timing: 5000,
+            useNativeDriver: true
+        }).start(async () => {
+            setShow(false);
+        });
+    }
+    
+    const animatedStyle = {
+        transform: moveAnim.getTranslateTransform()
+    }
+    // For coin animation
+
     return(
         <TouchableOpacity 
             onPress = {() => {
-                props.engine.dispatch({ type: "shake-tree", })
+                //props.engine.dispatch({ type: "shake-tree"})
+                setShow(true)
+                move()
             }}>
             <Image style={{ position: 'absolute', width: Dimensions.get("window").width * 0.1, height: Dimensions.get("window").height * 0.1}} resizeMode="stretch" source={tree} />
+            {
+                show 
+                ? <Animated.Image
+                    source={coinImage}
+                    style={[styles.coinImage, animatedStyle]}
+                    />
+                : <View />
+            }
         </TouchableOpacity>
             
     )
 }
+
+const styles = StyleSheet.create({
+    coinImage: {
+        height: 30,
+        width: 30,
+        marginTop: -3
+    }
+})
